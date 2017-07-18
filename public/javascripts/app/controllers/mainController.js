3be282434caf33ec71032dc0ab4f08e1d1452338
tylerh <tylerh@qualtrics.com>
2017-07-18 17:39:19 -0600

    added ability to add and remove stocks

--- public/javascripts/app/controllers/mainController.js
+++ public/javascripts/app/controllers/mainController.js
@@ -2,7 +2,48 @@
 
 	var mainController = function ($scope, $routeParams, $rootScope, $timeout, socket) {
 
-		// controller code here
+		$scope.stocks = [
+			{
+				name: "Google",
+				symbol: "GOOG",
+				price: 987.48
+			},
+			{
+				name: "Apple",
+				symbol: "AAPL",
+				price: 149.56
+			},
+			{
+				name: "Facebook",
+				symbol: "FB",
+				price: 163.06
+			},
+			{
+				name: "Twitter",
+				symbol: "TWTR",
+				price: 19.98
+			}
+		];
+
+		$scope.addStock = function () {
+			if (typeof $scope.stockSymbol != "undefined" && $scope.stockSymbol != "") {
+				$scope.stocks.push({
+					name: "name here",
+					symbol: $scope.stockSymbol,
+					price: 0
+				});
+				$scope.stockSymbol = "";
+			}
+		}
+
+		$scope.removeStock = function (symbol) {
+			for (var i = 0; i < $scope.stocks.length; i++) {
+				if ($scope.stocks[i].symbol == symbol) {
+					$scope.stocks.splice(i, 1);
+					break;
+				}
+			}
+		}
 
 	};
 
